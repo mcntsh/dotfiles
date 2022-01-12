9794395de5b73c28ca8efb73139614b685f5a0f3
Jon McIntosh <jonmcntsh@gmail.com>
2022-01-12 08:38:51 +0100

    Update prompts.zsh

--- zsh/.oh-my-zsh/custom/prompts.zsh
+++ zsh/.oh-my-zsh/custom/prompts.zsh
@@ -1,13 +1,1 @@
-# Golang
-export PATH=/usr/local/go/bin:/Users/jmcintosh/.deno/bin:$PATH
-
-# For the AWS CLI
-export N26_EMAIL=jon.mcintosh@n26.com
-
-# Z
-. $ZSH_CUSTOM/plugins/z/z.sh
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
+# export PATH=/usr/local/go/bin:$PATH
